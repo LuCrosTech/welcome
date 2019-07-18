48b3133083cbfec83b444a67c75edc0b0e3014b6
Luis Crosby <lcrosby@ncl.com>
2019-07-18 17:48:24 -0400

    [LA-17] First commit

---
+++ README.md
@@ -0,0 +1,13 @@
+<h3>Futuristic Android Application</h3>
+This android application shall run seamlesly to control any other
+flying device by accessing the low level TCP/IP layers, by
+injecting packets and datagrams, in order to allow a safe 
+landing and operation.<h3>Futuristic Android Application</h3>
+This android application shall run seamlesly to control any other
+flying device by accessing the low level TCP/IP layers, by
+injecting packets and datagrams, in order to allow a safe 
+landing and operation.<h3>Futuristic Android Application</h3>
+This android application shall run seamlesly to control any other
+flying device by accessing the low level TCP/IP layers, by
+injecting packets and datagrams, in order to allow a safe 
+landing and operation.
---
+++ app/src/main/java/com/tutorial/first/MainActivity.java
@@ -0,0 +1,31 @@
+package com.tutorial.first;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        getSupportActionBar().hide();
+//        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+        button1 = (Button) findViewById(R.id.button1);
+    }
+
+    public void onClick(View v) {
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        startActivity(intent);
+    }
+
+}
